6037b95668d5c97edddb0b9c42d0bba01578d199
tien2172t <tien2172t@gmail.com>
2025-11-18 19:18:41 +0700

    đầu

---
+++ introDTI/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DTI.Web.Controllers // Đổi namespace này theo tên Project của bạn (VD: IntroDTI.Controllers)
+{
+    public class AccountController : Controller
+    {
+        // Action để hiển thị trang Login
+        // Đường dẫn sẽ là: /Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // Action để xử lý khi người dùng bấm nút "Đăng nhập" (Submit form)
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Code xử lý đăng nhập sẽ viết ở đây sau này
+            // Tạm thời cứ cho về trang chủ
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ introDTI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace introDTI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
